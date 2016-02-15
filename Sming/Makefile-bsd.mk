39dfab8665302529f4be2a4f7a00eee5eb8dc4c8
Alexander V. Ribchansky <avr39ripe@gmail.com>
2016-02-15 16:13:38 +0200

    Add memanalyzer to Makefile-bsd

--- Sming/Makefile-bsd.mk
+++ Sming/Makefile-bsd.mk
@@ -13,4 +13,5 @@ ESPTOOL		 ?= $(ESP_HOME)/esptool/esptool.py
 KILL_TERM    ?= pkill -9 -f "$(COM_PORT) $(COM_SPEED_SERIAL)" || exit 0
 GET_FILESIZE ?= stat -f "%-15z"
 TERMINAL     ?= python -m serial.tools.miniterm $(COM_PORT) $(COM_SPEED_SERIAL)
-MEMANALYZER  ?= $(OBJDUMP) -h -j .data -j .rodata -j .bss -j .text -j .irom0.text
+#MEMANALYZER  ?= $(OBJDUMP) -h -j .data -j .rodata -j .bss -j .text -j .irom0.text
+MEMANALYZER  ?= ~/src/esp8266/esp-open-sdk/tools/memanalyzer $(OBJDUMP)
